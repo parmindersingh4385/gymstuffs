efb9aa3a3c4bc403fe544debad46075b12b8697c
Parminder singh <parmindersingh@sdplabs.com>
2025-03-04 19:10:05 +0530

    Commit changes

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -20,78 +20,96 @@ const productUrls = ref([
     { id: 15, title: "Skipping ropes", product_url: "/products/skippingropes" },
 ]);
 
-const images = ref([
+const storeUrls = ref([
     {
         src: "https://m.media-amazon.com/images/S/stores-image-uploads-eu-prod/5/AmazonStores/A21TJRUUN4KGV/e7eafc3586fcac05386e9e6bc68f03ca.w400.h400._CR0%2C0%2C400%2C400_SX200_.png",
-        name: "Image 1",
+        name: "TLISMI",
+        store_id: "tlismi",
     },
     {
         src: "https://m.media-amazon.com/images/S/al-eu-726f4d26-7fdb/f5a7f109-839e-4e9a-a2da-3f9beff0cf6c._CR0%2C0%2C3001%2C3001_SX200_.jpg",
-        name: "Image 2",
+        name: "HOMEGALLEY",
+        store_id: "homegalley",
     },
     {
-        src: "https://m.media-amazon.com/images/S/abs-image-upload-na/2/AmazonStores/A21TJRUUN4KGV/de38eafdf060ca3038560226f7b1f4f7.w1738.h1440._CR138%2C0%2C1440%2C1440_SX200_.png",
-        name: "Image 3",
+        src: "https://m.media-amazon.com/images/S/stores-image-uploads-eu-prod/5/AmazonStores/A21TJRUUN4KGV/e7eafc3586fcac05386e9e6bc68f03ca.w400.h400._CR0%2C0%2C400%2C400_SX200_.png",
+        name: "TLISMI",
+        store_id: "tlismi",
     },
     {
-        src: "https://asset20.ckassets.com/resources/image/stores/amazon-1735629515.jpg",
-        name: "Image 4",
+        src: "https://m.media-amazon.com/images/S/al-eu-726f4d26-7fdb/f5a7f109-839e-4e9a-a2da-3f9beff0cf6c._CR0%2C0%2C3001%2C3001_SX200_.jpg",
+        name: "HOMEGALLEY",
+        store_id: "homegalley",
     },
     {
-        src: "https://asset20.ckassets.com/resources/image/stores/amazon-1735629515.jpg",
-        name: "Image 5",
+        src: "https://m.media-amazon.com/images/S/stores-image-uploads-eu-prod/5/AmazonStores/A21TJRUUN4KGV/e7eafc3586fcac05386e9e6bc68f03ca.w400.h400._CR0%2C0%2C400%2C400_SX200_.png",
+        name: "TLISMI",
+        store_id: "tlismi",
     },
     {
-        src: "https://asset20.ckassets.com/resources/image/stores/amazon-1735629515.jpg",
-        name: "Image 6",
+        src: "https://m.media-amazon.com/images/S/al-eu-726f4d26-7fdb/f5a7f109-839e-4e9a-a2da-3f9beff0cf6c._CR0%2C0%2C3001%2C3001_SX200_.jpg",
+        name: "HOMEGALLEY",
+        store_id: "homegalley",
     },
     {
-        src: "https://asset20.ckassets.com/resources/image/stores/amazon-1735629515.jpg",
-        name: "Image 7",
+        src: "https://m.media-amazon.com/images/S/stores-image-uploads-eu-prod/5/AmazonStores/A21TJRUUN4KGV/e7eafc3586fcac05386e9e6bc68f03ca.w400.h400._CR0%2C0%2C400%2C400_SX200_.png",
+        name: "TLISMI",
+        store_id: "tlismi",
     },
     {
-        src: "https://asset20.ckassets.com/resources/image/stores/amazon-1735629515.jpg",
-        name: "Image 8",
+        src: "https://m.media-amazon.com/images/S/al-eu-726f4d26-7fdb/f5a7f109-839e-4e9a-a2da-3f9beff0cf6c._CR0%2C0%2C3001%2C3001_SX200_.jpg",
+        name: "HOMEGALLEY",
+        store_id: "homegalley",
     },
     {
-        src: "https://asset20.ckassets.com/resources/image/stores/amazon-1735629515.jpg",
-        name: "Image 9",
+        src: "https://m.media-amazon.com/images/S/stores-image-uploads-eu-prod/5/AmazonStores/A21TJRUUN4KGV/e7eafc3586fcac05386e9e6bc68f03ca.w400.h400._CR0%2C0%2C400%2C400_SX200_.png",
+        name: "TLISMI",
+        store_id: "tlismi",
     },
     {
-        src: "https://asset20.ckassets.com/resources/image/stores/amazon-1735629515.jpg",
-        name: "Image 10",
+        src: "https://m.media-amazon.com/images/S/al-eu-726f4d26-7fdb/f5a7f109-839e-4e9a-a2da-3f9beff0cf6c._CR0%2C0%2C3001%2C3001_SX200_.jpg",
+        name: "HOMEGALLEY",
+        store_id: "homegalley",
     },
     {
-        src: "https://asset20.ckassets.com/resources/image/stores/amazon-1735629515.jpg",
-        name: "Image 11",
+        src: "https://m.media-amazon.com/images/S/stores-image-uploads-eu-prod/5/AmazonStores/A21TJRUUN4KGV/e7eafc3586fcac05386e9e6bc68f03ca.w400.h400._CR0%2C0%2C400%2C400_SX200_.png",
+        name: "TLISMI",
+        store_id: "tlismi",
     },
     {
-        src: "https://asset20.ckassets.com/resources/image/stores/amazon-1735629515.jpg",
-        name: "Image 12",
+        src: "https://m.media-amazon.com/images/S/al-eu-726f4d26-7fdb/f5a7f109-839e-4e9a-a2da-3f9beff0cf6c._CR0%2C0%2C3001%2C3001_SX200_.jpg",
+        name: "HOMEGALLEY",
+        store_id: "homegalley",
     },
     {
-        src: "https://asset20.ckassets.com/resources/image/stores/amazon-1735629515.jpg",
-        name: "Image 13",
+        src: "https://m.media-amazon.com/images/S/stores-image-uploads-eu-prod/5/AmazonStores/A21TJRUUN4KGV/e7eafc3586fcac05386e9e6bc68f03ca.w400.h400._CR0%2C0%2C400%2C400_SX200_.png",
+        name: "TLISMI",
+        store_id: "tlismi",
     },
     {
-        src: "https://asset20.ckassets.com/resources/image/stores/amazon-1735629515.jpg",
-        name: "Image 14",
+        src: "https://m.media-amazon.com/images/S/al-eu-726f4d26-7fdb/f5a7f109-839e-4e9a-a2da-3f9beff0cf6c._CR0%2C0%2C3001%2C3001_SX200_.jpg",
+        name: "HOMEGALLEY",
+        store_id: "homegalley",
     },
     {
-        src: "https://asset20.ckassets.com/resources/image/stores/amazon-1735629515.jpg",
-        name: "Image 15",
+        src: "https://m.media-amazon.com/images/S/stores-image-uploads-eu-prod/5/AmazonStores/A21TJRUUN4KGV/e7eafc3586fcac05386e9e6bc68f03ca.w400.h400._CR0%2C0%2C400%2C400_SX200_.png",
+        name: "TLISMI",
+        store_id: "tlismi",
     },
     {
-        src: "https://asset20.ckassets.com/resources/image/stores/amazon-1735629515.jpg",
-        name: "Image 16",
+        src: "https://m.media-amazon.com/images/S/al-eu-726f4d26-7fdb/f5a7f109-839e-4e9a-a2da-3f9beff0cf6c._CR0%2C0%2C3001%2C3001_SX200_.jpg",
+        name: "HOMEGALLEY",
+        store_id: "homegalley",
     },
     {
-        src: "https://asset20.ckassets.com/resources/image/stores/amazon-1735629515.jpg",
-        name: "Image 17",
+        src: "https://m.media-amazon.com/images/S/stores-image-uploads-eu-prod/5/AmazonStores/A21TJRUUN4KGV/e7eafc3586fcac05386e9e6bc68f03ca.w400.h400._CR0%2C0%2C400%2C400_SX200_.png",
+        name: "TLISMI",
+        store_id: "tlismi",
     },
     {
-        src: "https://asset20.ckassets.com/resources/image/stores/amazon-1735629515.jpg",
-        name: "Image 18",
+        src: "https://m.media-amazon.com/images/S/al-eu-726f4d26-7fdb/f5a7f109-839e-4e9a-a2da-3f9beff0cf6c._CR0%2C0%2C3001%2C3001_SX200_.jpg",
+        name: "HOMEGALLEY",
+        store_id: "homegalley",
     },
 ]);
 
@@ -100,15 +118,16 @@ const router = useRouter();
 const goToProduct = (productUrl) => {
     router.push(productUrl);
 };
+
+const goToStore = (storeId) => {
+    router.push("/stores/" + storeId);
+};
 </script>
 
 <template>
-    <v-container fluid  class="d-flex justify-center">
-        <v-sheet
-            class="mx-auto text-center py-5"
-            max-width="100%"
-        >
-            <v-slide-group show-arrows >
+    <v-container fluid class="d-flex justify-center">
+        <v-sheet class="mx-auto text-center py-5" max-width="100%">
+            <v-slide-group show-arrows>
                 <v-slide-group-item
                     v-for="(product, index) in productUrls"
                     :key="index"
@@ -133,27 +152,35 @@ const goToProduct = (productUrl) => {
             </v-slide-group>
         </v-sheet>
     </v-container>
-    
+
     <div class="px-5">
-        <v-container fluid >
+        <v-container fluid>
             <v-row>
                 <v-col cols="12">
                     <p class="text-h5 font-weight-light">Popular stores</p>
                 </v-col>
             </v-row>
             <v-row>
-                <v-col class="position-relative" v-for="(image, index) in images" :key="index" cols="12" sm="6" md="4" lg="2">
+                <v-col
+                    class="position-relative"
+                    v-for="(store, index) in storeUrls"
+                    :key="index"
+                    cols="12"
+                    sm="6"
+                    md="4"
+                    lg="2"
+                    @click="goToStore(store.store_id)"
+                >
                     <v-card class="pa-4 h-100">
-                        <div class="w-50 mx-auto imgHeight d-flex align-center justify-center">
-                            <v-img
-                                :src="image.src"            
-                            />
+                        <div
+                            class="w-50 mx-auto imgHeight d-flex align-center justify-center"
+                        >
+                            <v-img :src="store.src" />
                         </div>
-                        <v-card-title>{{ image.name }}</v-card-title>
+                        <v-card-title>{{ store.name }}</v-card-title>
                     </v-card>
                 </v-col>
             </v-row>
         </v-container>
     </div>
 </template>
-
---
+++ src/pages/stores/[storeId].vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const storeName = ref(route.params.storeId);
+</script>
+
+<template>{{ storeName }}</template>
