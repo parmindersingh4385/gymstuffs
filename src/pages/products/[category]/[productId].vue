75a8976ccc6ca0ba91f75574cae1e39a141ac678
Parminder singh <parmindersingh@sdplabs.com>
2025-03-21 22:40:26 +0530

    Commit changes

---
+++ src/pages/products/[category]/[productId].vue
@@ -0,0 +1,10 @@
+<script setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+console.log(route.params.category);
+console.log(route.params.productId);
+</script>
+
+<template>Product detail.....................</template>
---
+++ src/pages/products/[category]/index.vue
@@ -0,0 +1,27 @@
+<script setup>
+import GlovesView from "@/views/products/GlovesView.vue";
+import RollersView from "@/views/products/RollersView.vue";
+import ShakersView from "@/views/products/ShakersView.vue";
+import SkippingRopesView from "@/views/products/SkippingRopesView.vue";
+import WristBandsView from "@/views/products/WristBandsView.vue";
+import { shallowRef } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const currentComponent = shallowRef(); // Default component
+
+const componentViews = {
+	shakers: ShakersView,
+	rollers: RollersView,
+	gloves: GlovesView,
+	wristbands: WristBandsView,
+	skippingropes: SkippingRopesView,
+};
+
+currentComponent.value = componentViews[route.params.category];
+</script>
+
+<template>
+	<component :is="currentComponent" />
+</template>
