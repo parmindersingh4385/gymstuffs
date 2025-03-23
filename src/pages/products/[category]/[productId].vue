d168d5ecf3597e855f3640d12b689700dc060bf6
Parminder singh <parmindersingh@sdplabs.com>
2025-03-23 23:24:10 +0530

    Commit changes

--- src/pages/products/[category]/[productId].vue
+++ src/pages/products/[category]/[productId].vue
@@ -3,8 +3,39 @@ import { useRoute } from "vue-router";
 
 const route = useRoute();
 
-console.log(route.params.category);
-console.log(route.params.productId);
+/* console.log(route.params.category);
+console.log(route.params.productId); */
 </script>
 
-<template>Product detail.....................</template>
+<template>
+	<v-container>
+		<v-sheet style="cursor: pointer">
+			<div style="width: 270px; height: 270px">
+				<v-img
+					src="https://asset20.ckassets.com/resources/image/ckseller/CKS-Health-Supplements-000293_1-1691578247.jpg"
+					alt="Profile"
+				/>
+			</div>
+			<div>
+				MuscleBlaze Biozyme Performance Whey, Labdoor USA Certified,
+				Chocolate Hazelnut
+			</div>
+			<div style="width: 120px; height: 120px">
+				<v-img
+					style="width: 80px; height: 40px"
+					src="https://asset20.ckassets.com/resources/image/stores/amazon-1735629515.jpg"
+				/>
+				<div>Discounted Price - ₹2,701</div>
+				<VBtn>Grab Deal</VBtn>
+			</div>
+			<div style="width: 120px; height: 120px">
+				<v-img
+					style="width: 80px; height: 40px"
+					src="https://asset20.ckassets.com/resources/image/stores/flipkart.png"
+				/>
+				<div>Discounted Price - ₹2,701</div>
+				<VBtn>Grab Deal</VBtn>
+			</div>
+		</v-sheet>
+	</v-container>
+</template>
