4d27ea81c5240d744cb4e1b00e214e653b535edd
Parminder singh <parmindersingh@sdplabs.com>
2025-04-07 07:42:30 +0530

    Commit changes

--- src/pages/product/add.vue
+++ src/pages/product/add.vue
@@ -1,3 +1,143 @@
-<script setup></script>
+<script setup>
+import { reactive, ref } from "vue";
+import axios from "axios";
 
-<template>Add product</template>
+const isLoading = ref(false);
+const formRef = ref(null);
+const productInfo = ref(null);
+
+const desserts = [
+	{
+		name: "Frozen Yogurt",
+		calories: 159,
+	},
+	{
+		name: "Ice cream sandwich",
+		calories: 237,
+	},
+	{
+		name: "Eclair",
+		calories: 262,
+	},
+	{
+		name: "Cupcake",
+		calories: 305,
+	},
+];
+
+// form Data
+const formData = reactive({
+	productId: null,
+});
+
+// 👉 Required Validator
+const requiredValidator = (value) => {
+	if (value === false || value === null) return "This field is required";
+
+	return !!String(value).trim().length || "This field is required";
+};
+
+const handleSubmit = async () => {
+	const validateForm = await formRef.value.validate();
+	const isFormValid = validateForm ? validateForm.valid : false;
+
+	if (isFormValid == false) {
+		return false;
+	}
+
+	const productId = formData.productId;
+
+	axios
+		.get(`http://localhost:3001/product/${productId}`)
+		.then((res) => {
+			formData.productId = null;
+			productInfo.value = res.data.productBaseInfoV1;
+			console.log(productInfo.value);
+		})
+		.catch((err) => console.error("Error:", err.response?.data || err));
+};
+
+const closeWindow = () => {};
+</script>
+
+<template>
+	<v-container
+		class="d-flex justify-center align-center py-0"
+		style="height: 100vh"
+	>
+		<v-card class="pa-4" width="400">
+			<VForm @submit.prevent="handleSubmit" ref="formRef">
+				<VRow>
+					<VCol cols="12">
+						<VTextField
+							v-model="formData.productId"
+							label="Product ID"
+							type="input"
+							:rules="[requiredValidator]"
+						></VTextField>
+					</VCol>
+				</VRow>
+				<!-- Update and Cancel Button -->
+				<VCardActions class="pt-4">
+					<VSpacer></VSpacer>
+					<!-- <VBtn
+						variant="tonal"
+						color="secondary"
+						@click="closeWindow"
+						size="small"
+						>Cancel</VBtn
+					> -->
+					<VBtn
+						:loading="isLoading"
+						type="submit"
+						variant="flat"
+						color="primary"
+						size="small"
+						>Add product</VBtn
+					>
+				</VCardActions>
+			</VForm>
+			<!-- shows products detail -->
+			<v-table v-if="productInfo">
+				<thead>
+					<tr>
+						<th class="text-left">Product information</th>
+						<th class="text-left">Value</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Product id</td>
+						<td>{{ productInfo.productId }}</td>
+					</tr>
+					<tr>
+						<td>Title</td>
+						<td>{{ productInfo.title }}</td>
+					</tr>
+					<tr>
+						<td>Image</td>
+						<td>
+							<VImg
+								class="py-4 px-4"
+								width="200"
+								height="200"
+								:src="productInfo.imageUrls['200x200']"
+							></VImg>
+						</td>
+					</tr>
+					<tr>
+						<td>Affilate url</td>
+						<td>
+							<a
+								:href="productInfo.productUrl"
+								target="_blank"
+								rel="noopener"
+								>Go to product</a
+							>
+						</td>
+					</tr>
+				</tbody>
+			</v-table>
+		</v-card>
+	</v-container>
+</template>
